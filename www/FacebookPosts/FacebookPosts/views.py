856ab729ad2ad1637ed88fa953654a3fb3f0ad93
Max <MaxMiller117@gmail.com>
2017-11-02 16:05:22 -0400

    Actually added the files

---
+++ www/FacebookPosts/FacebookPosts/views.py
@@ -0,0 +1,23 @@
+import psycopg2
+from flask import render_template
+
+from theapp import app
+
+@app.route('/')
+def index():
+    return '<h1>hello world!</h1>'
+
+@app.route('/query')
+def query():
+
+    conn_string = "host='localhost' dbname='mydb' user='dgillman' password='rufus'"
+    conn = psycopg2.connect(conn_string)
+    cursor = conn.cursor()
+    cursor.execute("select * from stations;")
+    records = cursor.fetchall()
+
+    return str(records)
+
+@app.route('/home')
+def home():
+    return render_template("home.html")
