a03e129c2e37f4485e2f295bb9f97e3f30bbfead
srive326 <srive326@Stephanies-MacBook-Pro.local>
2017-11-03 20:34:08 -0400

    stuff

--- www/FacebookPosts/FacebookPosts/flaskplotlib.py
+++ www/FacebookPosts/FacebookPosts/flaskplotlib.py
@@ -1,8 +1,8 @@
 from flask import Flask, make_response
 app = Flask(__name__)
 
-@app.route("/simple.png")
-def simple():
+@app.route("/weekly_followers.png")
+def weekly_followers():
     import datetime
     import StringIO
     import random
@@ -11,22 +11,54 @@ def simple():
     from matplotlib.figure import Figure
     from matplotlib.dates import DateFormatter
     from matplotlib.axes import Axes
+    import psycopg2
 
+
+    conn = psycopg2.connect("dbname=fb_data user=srive326")
+    cur = conn.cursor()
+
+    query = """CREATE OR REPLACE FUNCTION source_percent_change(parm1 text)
+      RETURNS TABLE (year double precision, weekly double precision, percent_change numeric)
+    AS
+    $body$
+      SELECT
+        a.year,a.weekly, round(((cast(a.followers AS numeric) - cast(a.lag AS numeric))/ NULLIF(cast(a.lag AS numeric),0)) * 100,4) AS percent_change 
+    FROM
+    (
+        SELECT *,lag(followers) OVER (partition BY source) AS lag FROM followers_weekly
+    )
+        AS a WHERE source = parm1;
+    $body$
+    language sql; """
+    cur.execute(query)
+
+    query = """
+    select year, weekly, percent_change from source_percent_change('Blacktivist')"""
+
+    cur.execute(query)
+
+    data = cur.fetchall()
+    
+    # Calculate weeks from the first observation
+    years, weeks, percent_changes = zip(*data)
+	
+    year0 = years[0]
+    week0 = weeks[0]
+	
+    weeks_from_start = []
+	
+    for i in range(len(years)):
+        weeks_from_start.append((years[i] - year0) * 52 + weeks[i] - week0)
+	
     fig=Figure()
     ax=fig.add_subplot(111)
-    x=[]
-    y=[]
-    ax.set_xlabel('Date')
-    ax.set_ylabel('Amount')
-    now=datetime.datetime.now()
-    delta=datetime.timedelta(days=1)
-    for i in range(10):
-        x.append(now)
-        now+=delta
-        y.append(random.randint(0, 1000))
-    ax.plot_date(x, y, '-')
-    ax.xaxis.set_major_formatter(DateFormatter('%Y-%m-%d'))
-    fig.autofmt_xdate()
+    ax.set_xlabel('Weeks From First Post')
+    ax.set_ylabel('Percent Change in Followers')
+    #ax.set_title()
+    
+    ax.plot(weeks_from_start, percent_changes)
+    #ax.xaxis.set_major_formatter(DateFormatter('%Y-%m-%d'))
+    #fig.autofmt_xdate()
     canvas=FigureCanvas(fig)
     png_output = StringIO.StringIO()
     canvas.print_png(png_output)
@@ -37,3 +69,4 @@ def simple():
 
 if __name__ == "__main__":
     app.run()
+    #weekly_followers()
---
+++ www/FacebookPosts/FacebookPosts/normalize_data.sql
@@ -0,0 +1,64 @@
+-- Delete tables if they already exist. Easier than a bunch of IF NOT EXISTS
+DROP TABLE IF EXISTS fb_data;
+DROP TABLE IF EXISTS posts CASCADE;
+DROP TABLE IF EXISTS pages CASCADE;
+DROP TABLE IF EXISTS page_followers CASCADE;
+DROP TABLE IF EXISTS followers_weekly CASCADE;
+
+-- Create table to hold the unnormalized data
+CREATE TABLE fb_data(
+id varchar, 
+source varchar, 
+page_url varchar,
+followers varchar,
+"date" date, 
+"time" time, 
+time_zone varchar,
+interactions varchar,
+post_type varchar, 
+post_message varchar, 
+post_link varchar
+);
+
+-- load unnormalized data from csv
+\copy fb_data from '/Users/srive326/Desktop/facebook_pages_moved.csv' with CSV HEADER
+
+-- Now start creating normalized tables
+
+-- create pages table which simply describes all pages of interest that exist
+CREATE TABLE pages(
+id serial primary key,
+name varchar,
+url varchar);
+
+INSERT INTO pages(name, url) SELECT source, page_url FROM fb_data GROUP BY source, page_url;
+
+
+-- create page_followers table to track followers for pages over time
+CREATE TABLE page_followers(
+page_id integer references pages(id),
+followers integer,
+"date" date,
+"time" time,
+time_zone varchar
+);
+
+INSERT INTO page_followers(page_id, followers, "date", "time", time_zone) SELECT p.id, fb.followers::integer, fb."date", fb."time", fb.time_zone FROM fb_data AS fb, pages AS p WHERE fb.source = p.name AND fb.page_url = p.url;
+
+
+-- create posts table describing specific posts to various facebook pages
+CREATE TABLE posts AS SELECT fb.id AS id, p.id AS page_id, "date", "time", time_zone, interactions, post_message, post_link FROM fb_data AS fb INNER JOIN pages AS p ON (fb.source = p.name AND fb.page_url = p.url);
+
+--ALTER TABLE posts ADD COLUMN page_id integer; 
+ALTER TABLE posts ADD CONSTRAINT fk_page_id FOREIGN KEY(page_id) REFERENCES pages(id);
+--UPDATE posts SET page_id = pages.id FROM pages WHERE source = pages.name AND fb_data.page_url = pages.url;
+
+ALTER TABLE posts ALTER COLUMN id SET DATA TYPE integer USING (id::integer);
+ALTER TABLE posts ADD CONSTRAINT pk_id PRIMARY KEY(id);
+
+--sql to make table with followers by week
+CREATE TABLE followers_weekly AS SELECT name AS source, max(followers) AS followers, date_part('year', "date"::date) AS year,
+	date_part('week', "date"::date) AS weekly
+	FROM page_followers INNER JOIN pages ON (page_followers.page_id = pages.id)
+	GROUP BY source, year, weekly
+	ORDER BY source, year, weekly;
